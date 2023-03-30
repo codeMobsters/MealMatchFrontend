f1bf84646772fc7b85fbe473241eda09c28ada48
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-03-30 15:31:43 +0200

    Make refresh possible

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,12 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import { useNavigate } from 'react-router-dom';
-import React from 'react';
-import PostRecipeDialog from './PostRecipeDialog';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import PostRecipeDialog from "./PostRecipeDialog";
 
 function HeaderComp() {
   const navigate = useNavigate();
@@ -16,20 +16,22 @@ function HeaderComp() {
   };
 
   function handleGoHome() {
-    navigate("/");
+    navigate("/", { state: { favoriteIdWhenRouted: 0 } });
     window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
+      top: 0,
+      left: 0,
+      behavior: "smooth",
     });
   }
   return (
-    <AppBar sx={{
-        bottom: {xs: 'auto', tablet: 'auto'},
-        display: {xs: 'block', tablet: 'block', desktop: 'none'},
-        position: 'fixed',
-        top : {xs: 0, tablet: 0}
-  }}>
+    <AppBar
+      sx={{
+        bottom: { xs: "auto", tablet: "auto" },
+        display: { xs: "block", tablet: "block", desktop: "none" },
+        position: "fixed",
+        top: { xs: 0, tablet: 0 },
+      }}
+    >
       <Container>
         <Toolbar disableGutters>
           <Typography
@@ -38,19 +40,26 @@ function HeaderComp() {
             onClick={() => handleGoHome()}
             sx={{
               mr: 2,
-              display: 'flex',
-              fontFamily: 'monospace',
+              display: "flex",
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'secondary.main',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "secondary.main",
+              textDecoration: "none",
             }}
           >
             MealMatch
           </Typography>
           <Box sx={{ flexGrow: 1 }}></Box>
-          <Avatar alt="userPicture" src="insert user avatar" onClick={() => handlePostAction()}/>
-          <PostRecipeDialog openPostDialog={openPostDialog} setOpenPostDialog={setOpenPostDialog} />
+          <Avatar
+            alt="userPicture"
+            src="insert user avatar"
+            onClick={() => handlePostAction()}
+          />
+          <PostRecipeDialog
+            openPostDialog={openPostDialog}
+            setOpenPostDialog={setOpenPostDialog}
+          />
         </Toolbar>
       </Container>
     </AppBar>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,14 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Box } from '@mui/material';
-import { LoginResponse, SetValue, EdamamResponse } from '../Utils/Types';
-import RecipeReviewCard from '../Components/RecipeCard';
-import Navbar from '../Components/Navbar';
-import '../App.css'
-import HeaderComp from '../Components/Header';
+import { Box } from "@mui/material";
+import { LoginResponse, SetValue, EdamamResponse } from "../Utils/Types";
+import RecipeReviewCard from "../Components/RecipeCard";
+import Navbar from "../Components/Navbar";
+import "../App.css";
+import HeaderComp from "../Components/Header";
 import { baseUrl } from "../Utils/Constants";
-
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 interface HomeProps {
   user: LoginResponse;
@@ -15,21 +16,28 @@ interface HomeProps {
 }
 
 const Home = (props: HomeProps) => {
-  const fetchRecipes = async ({pageParam = 0}) : Promise<EdamamResponse> => {
+  let { state } = useLocation();
+
+  const fetchRecipes = async ({ pageParam = 0 }): Promise<EdamamResponse> => {
     let res;
-    (pageParam !== 0 && hasNextPage) ?
-    res = await fetch(`${baseUrl}/api/Recipes/Edamam/Next?nextUrl=${encodeURIComponent(data!.pages.reverse()[0].url)}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${props.user.token}`,
-      },
-    }) :
-    res = await fetch(`${baseUrl}/api/Recipes/Edamam`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${props.user.token}`,
-      },
-    })
+    pageParam !== 0 && hasNextPage
+      ? (res = await fetch(
+          `${baseUrl}/api/Recipes/Edamam/Next?nextUrl=${encodeURIComponent(
+            data!.pages.reverse()[0].url
+          )}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${props.user.token}`,
+            },
+          }
+        ))
+      : (res = await fetch(`${baseUrl}/api/Recipes/Edamam`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${props.user.token}`,
+          },
+        }));
     return res.json();
   };
 
@@ -42,35 +50,43 @@ const Home = (props: HomeProps) => {
     isFetchingNextPage,
     status,
   } = useInfiniteQuery({
-    queryKey: ['recipes'],
+    queryKey: ["recipes"],
     refetchOnWindowFocus: false,
     queryFn: fetchRecipes,
     getNextPageParam: (lastPage, pages) => {
-        return lastPage.url;
-      }
+      return lastPage.url;
+    },
   });
+
+  useEffect(() => {
+    fetchNextPage();
+  }, [state]);
 
   return (
     <Box className="App">
       <HeaderComp />
-      <Navbar user={props.user} setUser={props.setUser}/>
-      <main style={{ width: "100%",
-        marginTop: '56px',
-        marginBottom: '56px' }}>
+      <Navbar user={props.user} setUser={props.setUser} />
+      <main style={{ width: "100%", marginTop: "56px", marginBottom: "56px" }}>
         {data && (
-          <InfiniteScroll threshold={1000} hasMore={hasNextPage} loadMore={() => fetchNextPage()} >
-            {data.pages.map((page) =>
-              page.recipes.map((recipe, index) => 
-              <RecipeReviewCard 
-                key={index} 
-                recipe={recipe}
-                user={props.user}
-              />)
+          <InfiniteScroll
+            threshold={1000}
+            hasMore={hasNextPage}
+            loadMore={() => fetchNextPage()}
+          >
+            {data.pages.map(page =>
+              page.recipes.map((recipe, index) => (
+                <RecipeReviewCard
+                  key={index}
+                  recipe={recipe}
+                  user={props.user}
+                />
+              ))
             )}
-          </InfiniteScroll>)}
+          </InfiniteScroll>
+        )}
       </main>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
