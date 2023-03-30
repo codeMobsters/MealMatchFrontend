eda6cb4750ff4670f275433b41ba08a068569f7f
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-03-30 14:01:59 +0200

    Not much
    Co-authored-by: Lajos Horvath <HorvathLajos@users.noreply.github.com>

--- src/Utils/Constants.ts
+++ src/Utils/Constants.ts
@@ -1,5 +1,6 @@
 export const Preferences = {
-    Cousines : ["american",
+  Cousines: [
+    "american",
     "asian",
     "british",
     "caribbean",
@@ -19,79 +20,82 @@ export const Preferences = {
     "nordic",
     "south american",
     "south east asian",
-    "world"],
-    DishTypes: ["alcohol cocktail"
-        ,"biscuits and cookies"
-        ,"bread"
-        ,"cereals"
-        ,"condiments and sauces"
-        ,"desserts"
-        ,"drinks"
-        ,"egg"
-        ,"ice cream and custard"
-        ,"main course"
-        ,"pancake"
-        ,"pasta"
-        ,"pastry"
-        ,"pies and tarts"
-        ,"pizza"
-        ,"preps"
-        ,"preserve"
-        ,"salad"
-        ,"sandwiches"
-        ,"seafood"
-        ,"side dish"
-        ,"soup"
-        ,"special occasions"
-        ,"starter"
-        ,"sweets"],
-    mealTypes: ["breakfast",
-            "brunch",
-            "lunch/dinner",
-            "snack",
-            "teatime"],
-    dietLabels: ["balanced",
+    "world",
+  ],
+  DishTypes: [
+    "alcohol cocktail",
+    "biscuits and cookies",
+    "bread",
+    "cereals",
+    "condiments and sauces",
+    "desserts",
+    "drinks",
+    "egg",
+    "ice cream and custard",
+    "main course",
+    "pancake",
+    "pasta",
+    "pastry",
+    "pies and tarts",
+    "pizza",
+    "preps",
+    "preserve",
+    "salad",
+    "sandwiches",
+    "seafood",
+    "side dish",
+    "soup",
+    "special occasions",
+    "starter",
+    "sweets",
+  ],
+  mealTypes: ["breakfast", "brunch", "lunch/dinner", "snack", "teatime"],
+  dietLabels: [
+    "balanced",
     "high-fiber",
     "high-protein",
     "low-carb",
     "low-fat",
-    "low-sodium"],
-    healthLabels: [	"alcohol-cocktail"
-        	,"alcohol-free"
-        	,"celery-free"
-        	,"crustacean-free"
-        	,"dairy-free"
-        	,"DASH"
-        	,"egg-free"
-        	,"fish-free"
-        	,"fodmap-free"
-        	,"gluten-free"
-        	,"immuno-supportive"
-        	,"keto-friendly"
-        	,"kidney-friendly"
-        	,"kosher"
-        	,"low-potassium"
-        	,"low-sugar"
-        	,"lupine-free"
-        	,"Mediterranean"
-        	,"mollusk-free"
-        	,"mustard-free"
-        	,"No-oil-added"
-        	,"paleo"
-        	,"peanut-free"
-        	,"pecatarian"
-        	,"pork-free"
-        	,"red-meat-free"
-        	,"sesame-free"
-        	,"shellfish-free"
-        	,"soy-free"
-        	,"sugar-conscious"
-        	,"sulfite-free"
-        	,"tree-nut-free"
-        	,"vegan"
-        	,"vegetarian"
-        	,"wheat-free"]
-}
+    "low-sodium",
+  ],
+  healthLabels: [
+    "alcohol-cocktail",
+    "alcohol-free",
+    "celery-free",
+    "crustacean-free",
+    "dairy-free",
+    "DASH",
+    "egg-free",
+    "fish-free",
+    "fodmap-free",
+    "gluten-free",
+    "immuno-supportive",
+    "keto-friendly",
+    "kidney-friendly",
+    "kosher",
+    "low-potassium",
+    "low-sugar",
+    "lupine-free",
+    "Mediterranean",
+    "mollusk-free",
+    "mustard-free",
+    "No-oil-added",
+    "paleo",
+    "peanut-free",
+    "pecatarian",
+    "pork-free",
+    "red-meat-free",
+    "sesame-free",
+    "shellfish-free",
+    "soy-free",
+    "sugar-conscious",
+    "sulfite-free",
+    "tree-nut-free",
+    "vegan",
+    "vegetarian",
+    "wheat-free",
+  ],
+};
 
-export const baseUrl = 'https://localhost:7031';
+export const baseUrl = "https://localhost:7031";
 // export const baseUrl = 'https://mealmatchapi.azurewebsites.net'
--- src/Utils/Types.ts
+++ src/Utils/Types.ts
@@ -1,96 +1,95 @@
 import { Dispatch, SetStateAction } from "react";
 
-
-export type SetValue<T> = Dispatch<SetStateAction<T>>
+export type SetValue<T> = Dispatch<SetStateAction<T>>;
 
 export interface LoginRequest {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export interface RegisterRequest {
-    username: string;
-    name: string;
-    password: string;
+  username: string;
+  name: string;
+  password: string;
 }
 
 export interface LoginResponse {
-    id: number;
-    name: string;
-    token: string;
+  id: number;
+  name: string;
+  token: string;
 }
 export interface NavbarProps {
-    user: LoginResponse;
-    setUser: SetValue<LoginResponse>;
-};
+  user: LoginResponse;
+  setUser: SetValue<LoginResponse>;
+}
 
 export interface EdamamResponse {
-    url: string
-    recipes: Recipe[]
-  }
+  url: string;
+  recipes: Recipe[];
+}
 
 export interface FavoriteRecipe {
-  favoriteRecipeId: number
-  recipe: Recipe
-  favoriteRecipeOwnerId: number
-  favoriteRecipeOwnerName: string
+  favoriteRecipeId: number;
+  recipe: Recipe;
+  favoriteRecipeOwnerId: number;
+  favoriteRecipeOwnerName: string;
 }
 
 export interface Recipe {
-  recipeId?: number
-  label: string
-  like?: number
-  yield?: number
-  calories?: number
-  totalTime?: number
-  createdAt?: string
-  instructions?: string[]
-  ingredientLines: string[]
-  cuisineType?: string[]
-  dietLabels?: string[]
-  dishType?: string[]
-  healthLabels?: string[]
-  mealType?: string[]
-  image: string
-  url?: string
-  recipeOwnerId: number
-  recipeOwnerName?: string
-  comments?: Comment[]
+  recipeId?: number;
+  label: string;
+  like?: number;
+  yield?: number;
+  calories?: number;
+  totalTime?: number;
+  createdAt?: string;
+  instructions?: string[];
+  ingredientLines: string[];
+  cuisineType?: string[];
+  dietLabels?: string[];
+  dishType?: string[];
+  healthLabels?: string[];
+  mealType?: string[];
+  image: string;
+  url?: string;
+  recipeOwnerId: number;
+  recipeOwnerName?: string;
+  comments?: Comment[];
 }
-  
+
 export interface Comment {
-  commentId: number
-  commentText: string
-  commentAt: string
-  recipeId: number
-  userName: string
-  userProfilePictureUrl: any
+  commentId: number;
+  commentText: string;
+  commentAt: string;
+  recipeId: number;
+  userName: string;
+  userProfilePictureUrl: any;
 }
 
 export interface User {
-  userId: number
-  name: string
-  profilePictureUrl: string
-  profileSettings: string[]
-  dietLabels: string[]
-  healthLabels: string[]
+  userId: number;
+  name: string;
+  profilePictureUrl: string;
+  profileSettings: string[];
+  dietLabels: string[];
+  healthLabels: string[];
 }
 
 export interface NewRecipe {
-  Title: string
-  Yield?: string
-  Calories?: string
-  TotalTime?: string
-  Instructions: string[]
-  Ingredients: string[]
-  CuisineType?: string[]
-  DietLabels?: string[]
-  DishType?: string[]
-  HealthLabels?: string[]
-  MealType?: string[]
-  RecipePicture?: File
+  Title: string;
+  Yield?: string;
+  Calories?: string;
+  TotalTime?: string;
+  Instructions: string[];
+  Ingredients: string[];
+  CuisineType?: string[];
+  DietLabels?: string[];
+  DishType?: string[];
+  HealthLabels?: string[];
+  MealType?: string[];
+  RecipePicture?: File;
 }
 export type PostRecipeFormProps = {
-  formState :NewRecipe,
-  setFormState :React.Dispatch<React.SetStateAction<NewRecipe>>,
-}
+  formState: NewRecipe;
+  setFormState: React.Dispatch<React.SetStateAction<NewRecipe>>;
+};
